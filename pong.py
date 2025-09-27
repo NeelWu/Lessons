fc700cccead51bd049bd2b105280c482cb1fb1ce
NeelWu <110783268+NeelWu@users.noreply.github.com>
2025-09-27 20:10:08 +0000

    Added second paddle and Ball

--- pong.py
+++ pong.py
@@ -1,5 +1,10 @@
 import pygame
 
+PLAYER_WIDTH = 25
+PLAYER_HEIGHT = 100
+
+BALL_WIDTH = 20
+
 # pygame setup
 pygame.init()
 screen = pygame.display.set_mode((1280, 720))
@@ -7,7 +12,9 @@ clock = pygame.time.Clock()
 running = True
 dt = 0
 
-player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+player_pos = pygame.Vector2(screen.get_width() * 0.9, screen.get_height() / 2)
+player2_pos = pygame.Vector2(screen.get_width() * 0.1 - PLAYER_WIDTH, screen.get_height() / 2)
+ball_pos = pygame.Vector2(screen.get_width()/2, screen.get_height()/2)
 
 while running:
     # poll for events
@@ -17,19 +24,36 @@ while running:
             running = False
 
     # fill the screen with a color to wipe away anything from last frame
-    screen.fill("purple")
+    screen.fill("black")
 
-    pygame.draw.rectangle(screen, "red", player_pos, 40)
+    pygame.draw.rect(screen, "red", (*player_pos, PLAYER_WIDTH, PLAYER_HEIGHT))
+    pygame.draw.rect(screen, "blue", (*player2_pos, PLAYER_WIDTH, PLAYER_HEIGHT))
+    pygame.draw.circle(screen, "white", (*ball_pos, BALL_WIDTH, BALL_WIDTH))
 
     keys = pygame.key.get_pressed()
     if keys[pygame.K_w]:
         player_pos.y -= 300 * dt
     if keys[pygame.K_s]:
         player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
+    
+    if player_pos.y > screen.get_height() -PLAYER_HEIGHT:
+        player_pos.y = screen.get_height() - PLAYER_HEIGHT
+    elif player_pos.y < 0:
+        player_pos.y = 0
+
+  
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP]:
+        player2_pos.y -= 300 * dt
+    if keys[pygame.K_DOWN]:
+        player2_pos.y += 300 * dt
+    
+    if player2_pos.y > screen.get_height() -PLAYER_HEIGHT:
+        player2_pos.y = screen.get_height() - PLAYER_HEIGHT
+    elif player2_pos.y < 0:
+        player2_pos.y = 0
+
 
     # flip() the display to put your work on screen
     pygame.display.flip()
